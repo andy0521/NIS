7b5dd652ed251e1a8c32607d97cd3d69c6bba5f3
Andy Wu <andy132888@gmail.com>
2020-05-23 14:19:41 +0800

    登入介面完成

--- myapp/myapp/routes/login.js
+++ myapp/myapp/routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'login' });
+    res.render('login', { title: '護理控台' });
   });
   
   module.exports = router;
