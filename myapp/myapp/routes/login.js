2c3fdfa1493295abe9e09aeec1b04a4d90c61feb
Andy Wu <andy132888@gmail.com>
2020-05-23 17:02:19 +0800

    主頁面

--- myapp/myapp/routes/login.js
+++ myapp/myapp/routes/login.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('login', { title: '護理控台' });
+
+router.post('/', function(req, res, next) {
+    res.render('login', { title: 'login' });
   });
   
   module.exports = router;
