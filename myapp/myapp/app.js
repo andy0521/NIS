f78247fe552a5f5274fd21b9c1132d2fdac98475
Andy Wu <andy132888@gmail.com>
2020-05-23 20:23:18 +0800

    20200523

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login')
-var testRouter = require ('./routes/test')
+var loginRouter = require('./routes/login');
+
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-app.use('/test' , testRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
