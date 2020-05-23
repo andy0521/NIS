66556fed817734d3d867675a5a682ca31f0be1c2
Andy Wu <andy132888@gmail.com>
2020-05-23 19:02:15 +0800

    登入

--- myapp/myapp/routes/index.js
+++ myapp/myapp/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.render('index', { title: '護理控台首頁' });
+  res.render('index', { title: '護理控台首頁'});
+  
 });
 
 module.exports = router;
