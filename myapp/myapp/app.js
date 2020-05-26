3b099dfb084c5c8f1a93bbcccb8538c846258f07
Andy Wu <andy132888@gmail.com>
2020-05-26 19:36:16 +0800

    20200526假登入完成
    自動導進登入畫面

--- myapp/myapp/app.js
+++ myapp/myapp/app.js
@@ -3,17 +3,56 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
+var session = require('express-session');
+var bodyparser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyparser.json()); // 使用bodyparder中间件，
+app.use(bodyparser.urlencoded({ extended: true }));
+// 使用 session 中间件
+app.use(session({
+    secret :  'secret', // 对session id 相关的cookie 进行签名
+    resave : true,
+    saveUninitialized: false, // 是否保存未初始化的会话
+    cookie : {
+        maxAge : 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+    },
+}));
+// 获取登录页面
+app.get('/login', function(req, res){
+  res.sendFile(__dirname,'views' + '/login.html')
+});
+
+app.post('/login', function(req, res){
+  if(req.body.username == 'admin' && req.body.pwd == 'admin123'){
+      req.session.userName = req.body.username; // 登录成功，设置 session
+      res.redirect('/');
+  }
+  else{
+      res.json({ret_code : 1, ret_msg : '账号或密码错误'});// 若登录失败，重定向到登录页面
+  }
+});
+
+// 获取主页
+app.get('/', function (req, res) {
+  if(req.session.userName){  //判断session 状态，如果有效，则返回主页，否则转到登录页面
+      res.render('index',{username : req.session.userName});
+  }else{
+      res.redirect('login');
+  }
+})
+app.get('/logout', function (req, res) {
+  req.session.userName = null; // 删除session
+  res.redirect('login');
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- myapp/myapp/routes/login.js
+++ myapp/myapp/routes/login.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./users').items;
+ 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('login', { title: '護理控台' });
   });
+  
   module.exports = router;
  
