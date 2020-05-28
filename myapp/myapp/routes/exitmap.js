82c1021461cf5c8adc59e0a36e27ff577337c4c4
purplesunset871019 <hsb41202@gmail.com>
2020-05-28 16:40:38 +0800

    逃生資訊、公告訊息
    逃生訊息新增圖，公告訊息新增護理站訊息跟溫馨提醒頁面

---
+++ myapp/myapp/routes/exitmap.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/exitmap', function(req, res, next) {
+  res.render('exitmap', { title: '逃生平面圖' });
+});
+
+module.exports = router;
---
+++ myapp/myapp/routes/messsage.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/message', function(req, res, next) {
+  res.render('message', { title: '護理站訊息編輯' });
+});
+
+module.exports = router;
